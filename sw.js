fcb0b8bc57a18e99edd9b28a8365788a6db3e9ad
Marcel ZOHOU <zohoumax@gmail.com>
2026-06-25 22:46:10 +0100

    Salle de Jeux SALVE - Application PWA

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE = 'salve-app-v1';
+const FILES = [
+    './index.html',
+    './manifest.json',
+    './icon-192.png',
+    './icon-512.png'
+];
+
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open(CACHE).then(function(cache) {
+            return cache.addAll(FILES);
+        })
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+    e.waitUntil(
+        caches.keys().then(function(keys) {
+            return Promise.all(
+                keys.filter(function(k) { return k !== CACHE; })
+                    .map(function(k) { return caches.delete(k); })
+            );
+        })
+    );
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+    e.respondWith(
+        caches.match(e.request).then(function(r) {
+            return r || fetch(e.request).catch(function() {
+                return caches.match('./index.html');
+            });
+        })
+    );
+});
